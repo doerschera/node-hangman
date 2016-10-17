891b7452ae97049194a1ef061715cbd7dd730880
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-10-17 17:31:41 -0500

    add letters to blanks

--- letter.js
+++ letter.js
@@ -1,22 +1,21 @@
 
 function Letter(word, letter, indexes) {
+  this.win = false;
   this.blanks = [];
-  this.getBlanks = function() {
-    for(var i = 0; i < word.length; i++) {
-      this.blanks.push('_');
-    }
-  };
   this.getLetters = function() {
     for(var i = 0; i < indexes.length; i++) {
       var letterIndex = indexes[i];
       this.blanks[letterIndex] = letter;
     }
+    this.printBlanks();
   };
   this.printBlanks = function() {
     console.log(this.blanks.join(' '));
   }
-  this.win = function() {
-    if(this.blanks.findIndex('_') == -1) {
+  this.isWin = function() {
+    console.log(this.blanks.join());
+    if(this.blanks.join() == word) {
+      this.win = true;
       console.log('\n---------------');
       console.log('Yay! You win!');
       console.log('---------------\n');
--- main.js
+++ main.js
@@ -2,29 +2,40 @@ var Letter = require('./letter.js');
 var word = require('./word.js');
 var Game = require('./game.js');
 var inquirer = require('inquirer');
+var currentState = [];
 
 console.log(word);
-var blanks = '';
 
 for(var i = 0; i < word.length; i++) {
-  blanks += '_ ';
+  currentState.push('_');
 }
-console.log('\n'+ blanks +'\n');
+console.log('\n'+ currentState.join(' ') +'\n');
 
-inquirer.prompt([
-  {
-    message: 'Choose a letter.',
-    name: 'letter'
-  }
-]).then(function(input) {
-  var guess = input.letter;
-  var newGame = new Game(guess);
-  var correctLetters;
+function play() {
+  inquirer.prompt([
+    {
+      message: 'Choose a letter.',
+      name: 'letter'
+    }
+  ]).then(function(input) {
+    var guess = input.letter;
+    var newGame = new Game(guess);
+    var correctLetters;
 
-  newGame.word = word.split('');
-  newGame.check();
-  correctLetters = newGame.indexes
+    newGame.word = word.split('');
+    newGame.check();
+    correctLetters = newGame.indexes
 
-  var letterCheck = new Letter(word, guess, correctLetters);
-  letterCheck.getBlanks();
-})
+    var letterCheck = new Letter(word, guess, correctLetters);
+    letterCheck.blanks = currentState;
+    letterCheck.getLetters();
+    currentState = letterCheck.blanks;
+    console.log(currentState);
+
+    if(letterCheck.win == false) {
+      play();
+    }
+  })
+}
+
+play();
